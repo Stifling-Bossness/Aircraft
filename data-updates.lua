89985acd508a9356eb7e7b3e41522ea0c975c66e
Stifling_Bossness <notyouremail55@gmail.com>
2017-05-13 20:24:17 -0400

    1.3.5 - I broke bob's compatibility last version
    Fixed compatibility I broke in a way I didn't even know was possible

--- data-updates.lua
+++ data-updates.lua
@@ -16,22 +16,40 @@ end
 --Thanks to Articulating for this one :)
 --Updates Gunship, Jet, and Flying Fortress recipes to use Rifles instead of Submachine Guns.
 if data.raw["recipe"]["rifle"] then 
-    for i, ingredient in pairs(data.raw["recipe"]["gunship"].ingredients) do
+    for i, ingredient in pairs(data.raw["recipe"]["gunship"]["normal"].ingredients) do
         if ingredient.name == "submachine-gun" or ingredient[1] == "submachine-gun" then
-            table.remove(data.raw["recipe"]["gunship"].ingredients, i)
+            table.remove(data.raw["recipe"]["gunship"]["normal"].ingredients, i)
         end
     end
-	table.insert(data.raw["recipe"]["gunship"].ingredients, {"rifle", 5})
-	for i, ingredient in pairs(data.raw["recipe"]["jet"].ingredients) do
+	for i, ingredient in pairs(data.raw["recipe"]["gunship"]["expensive"].ingredients) do
 		if ingredient.name == "submachine-gun" or ingredient[1] == "submachine-gun" then
-			table.remove(data.raw["recipe"]["jet"].ingredients, i)
+			table.remove(data.raw["recipe"]["gunship"]["expensive"].ingredients, i)
 		end
 	end
-	table.insert(data.raw["recipe"]["jet"].ingredients, {"rifle", 3})
-	for i, ingredient in pairs(data.raw["recipe"]["flying-fortress"].ingredients) do
+	table.insert(data.raw["recipe"]["gunship"]["normal"].ingredients, {"rifle", 5})
+	table.insert(data.raw["recipe"]["gunship"]["expensive"].ingredients, {"rifle", 10})
+	for i, ingredient in pairs(data.raw["recipe"]["jet"]["normal"].ingredients) do
 		if ingredient.name == "submachine-gun" or ingredient[1] == "submachine-gun" then
-			table.remove(data.raw["recipe"]["flying-fortress"].ingredients, i)
+			table.remove(data.raw["recipe"]["jet"]["normal"].ingredients, i)
 		end
 	end
-	table.insert(data.raw["recipe"]["flying-fortress"].ingredients, {"rifle", 15})
+	for i, ingredient in pairs(data.raw["recipe"]["jet"]["expensive"].ingredients) do
+		if ingredient.name == "submachine-gun" or ingredient[1] == "submachine-gun" then
+			table.remove(data.raw["recipe"]["jet"]["expensive"].ingredients, i)
+		end
+	end
+	table.insert(data.raw["recipe"]["jet"]["normal"].ingredients, {"rifle", 3})
+	table.insert(data.raw["recipe"]["jet"]["expensive"].ingredients, {"rifle", 6})
+	for i, ingredient in pairs(data.raw["recipe"]["flying-fortress"]["normal"].ingredients) do
+		if ingredient.name == "submachine-gun" or ingredient[1] == "submachine-gun" then
+			table.remove(data.raw["recipe"]["flying-fortress"]["normal"].ingredients, i)
+		end
+	end
+	for i, ingredient in pairs(data.raw["recipe"]["flying-fortress"]["expensive"].ingredients) do
+		if ingredient.name == "submachine-gun" or ingredient[1] == "submachine-gun" then
+			table.remove(data.raw["recipe"]["flying-fortress"]["expensive"].ingredients, i)
+		end
+	end
+	table.insert(data.raw["recipe"]["flying-fortress"]["normal"].ingredients, {"rifle", 15})
+	table.insert(data.raw["recipe"]["flying-fortress"]["expensive"].ingredients, {"rifle", 30})
 end
